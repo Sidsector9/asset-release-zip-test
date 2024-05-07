2d0c250993b2b0b9adbeff643158e55060ed6910
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2024-05-07 15:22:14 +0530

    add deploy shell script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Note that this does not use pipefail
+# because if the grep later doesn't match any deleted files,
+# which is likely to be the case the majority of the time,
+# it does not exit with 0, as we are interested in the final exit.
+set -eo
+
+generate_zip() {
+    echo "Generating zip file..."
+
+    # use a symbolic link so the directory in the zip matches the slug
+    zip -r "${GITHUB_WORKSPACE}/${SLUG}.zip" "$SLUG"
+
+    echo "zip-path=${GITHUB_WORKSPACE}/${SLUG}.zip" >> "${GITHUB_OUTPUT}"
+    echo "✓ Zip file generated!"
+}
+
+generate_zip
+
+echo "✓ Plugin deployed!"
